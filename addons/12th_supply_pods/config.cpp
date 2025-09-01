671d3a1ca1dbaebe5f8b07f4b82b0cc5c9c28d23
12thMeu <12thmarineexpeditionary@gmail.com>
2025-09-01 13:44:58 +0100

    MG sup pod updated for new ammo

--- addons/12th_supply_pods/config.cpp
+++ addons/12th_supply_pods/config.cpp
@@ -246,9 +246,9 @@ class CfgVehicles {
     class TransportMagazines {
       class _xx_twelfth_100Rnd_95x40_Box { magazine = "twelfth_100Rnd_95x40_Box"; count = 10; };
       class _xx_twelfth_100Rnd_95x40_Box_T { magazine = "twelfth_100Rnd_95x40_Box_T"; count = 10; };
-      class _xx_twelfth_200Rnd_762x51_Box { magazine = "twelfth_200Rnd_762x51_Box"; count = 10; };
-      class _xx_twelfth_200Rnd_762x51_Box_T { magazine = "twelfth_200Rnd_762x51_Box_T"; count = 10; };
-      class _xx_twelfth_762x51_400rnd_T { magazine = "OPTRE_400Rnd_762x51_Box_Tracer"; count = 5; };
+      class _xx_Twelft_M739_SAW_200Rnd_Drum { magazine = "Twelft_M739_SAW_200Rnd_Drum"; count = 10; };
+      class _xx_OPTRE_200Rnd_762x51_M118_M247A1_Box { magazine = "OPTRE_200Rnd_762x51_M118_M247A1_Box"; count = 10; };
+      class _xx_OPTRE_400Rnd_762x51_M118_M247A1_Box { magazine = "OPTRE_400Rnd_762x51_M118_M247A1_Box"; count = 5; };
       class _xx_ACE_SpareBarrel { magazine = "ACE_SpareBarrel"; count = 5; };
     };
     class TransportWeapons {};
