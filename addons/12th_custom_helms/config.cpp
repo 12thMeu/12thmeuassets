aa25594b2acd91563302d975c67e099d96491dc5
Samantha <12thmarineexpeditionary@gmail.com>
2025-10-13 18:50:06 +0200

    simmons helmet

--- addons/12th_custom_helms/config.cpp
+++ addons/12th_custom_helms/config.cpp
@@ -196,6 +196,7 @@ class CfgWeapons {
   CUSTOM_HELM_S_AV(Rex)
   CUSTOM_HELM_S_AV(Sammy)
   CUSTOM_HELM_S_AV(Schultz)
+  CUSTOM_HELM_S_AV(Simmons)
   CUSTOM_HELM_S_AV(Sullivan)
   CUSTOM_HELM_S_AV(Felix)
   CUSTOM_HELM_S_AV(Kiens)
